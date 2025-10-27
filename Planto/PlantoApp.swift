26591bc1573d2a8f65d06672f3449d0b26e32427
SarahAlnasser <sarahalnasser04@gmail.com>
2025-10-27 16:42:51 +0300

    Initial Commit

---
+++ Planto/PlantoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantoApp.swift
+//  Planto
+//
+//  Created by Sarah Alnasser on 27/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct PlantoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
